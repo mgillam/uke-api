409e28dec85f2ca9aa9ed7c5b2479567a4d725c4
Mic <mic@secureideas.com>
2019-03-10 21:38:09 -0400

    WIP bearer token middleware

--- src/router/bearerAuth.router.js
+++ src/router/bearerAuth.router.js
@@ -10,6 +10,37 @@ routeSet.post('/auth', (req, res, next) => {
   return next();
 });
 
-routeSet.get('/status', (req, res, next) => {});
+routeSet.get('/status', extractAuth, (req, res, next) => {
+  res.json({
+    status: 'Success',
+    token: req.context.authToken
+  });
+  return next();
+});
+
+function extractAuth(req, res, next) {
+  let header = req.header('Authorization');
+  if (header) {
+    header = header.split(' ');
+    if (header[0] === 'Bearer') {
+      if (header[1]) {
+        req.context = Object.assign(req.context || {}, {
+          authToken: header[1]
+        });
+        return next();
+      } else {
+        //Invalid - no token value
+      }
+    } else {
+      //Invalid - only Bearer is supported
+    }
+  } else {
+    //Invalid - no auth header supplied
+  }
+  return res.json(401, {
+    status: 'Error',
+    message: 'No valid auth supplied'
+  });
+}
 
 module.exports = routeSet;
