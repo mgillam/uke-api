2ad629b2e42a9a6073f0b4bac5fc93e030cb5919
Mic <mic@secureideas.com>
2019-03-10 21:43:54 -0400

    typo fix in cookie auth unit test

--- test/router/cookieAuth.js
+++ test/router/cookieAuth.js
@@ -1,7 +1,7 @@
 const router = require('../../src/router/cookieAuth.router');
 const { spy } = require('sinon');
 
-describe('#bearerAuthRouter', function() {
+describe('#cookieAuthRouter', function() {
   it.skip('should return a set-cookie header with a jwt on auth', function() {});
   it.skip('should show status when token is valid');
   it.skip('should return an error when token is invalid');
